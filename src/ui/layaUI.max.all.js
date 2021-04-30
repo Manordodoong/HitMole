11ae91b078b232cfb3c1abc74309a92cbde82439
Manordodoong <caijuba19723@126.com>
2021-04-30 18:19:45 +0800

    proj init

---
+++ src/ui/layaUI.max.all.js
@@ -0,0 +1,26 @@
+var CLASS$=Laya.class;
+var STATICATTR$=Laya.static;
+var View=laya.ui.View;
+var Dialog=laya.ui.Dialog;
+var GameUI=(function(_super){
+		function GameUI(){
+			
+		    this.bg=null;
+		    this.normal=null;
+		    this.hit=null;
+
+			GameUI.__super.call(this);
+		}
+
+		CLASS$(GameUI,'ui.GameUI',_super);
+		var __proto__=GameUI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(GameUI.uiView);
+
+		}
+
+		GameUI.uiView={"type":"View","props":{"width":800,"height":600},"child":[{"type":"Image","props":{"y":25,"x":25,"var":"bg","skin":"comp/back.png","name":"bg"}},{"type":"Box","props":{"y":196,"x":154},"child":[{"type":"Image","props":{"x":3,"var":"normal","skin":"comp/mouse_normal_1.png"}},{"type":"Image","props":{"y":9,"x":2,"skin":"comp/mouse_hit_1.png"}},{"type":"Image","props":{"y":87,"var":"hit","skin":"comp/mask-01.png"}}]}]};
+		return GameUI;
+	})(View);
